4f001d1d440b64aea7f088b5bec7b55e614634bd
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2021-03-01 23:35:04 +0100

    v1.0

---
+++ js/functions.js
@@ -0,0 +1,183 @@
+/**
+ * Theme functions file
+ *
+ * Contains handlers for navigation, accessibility, header sizing
+ * footer widgets and Featured Content slider
+ *
+ */
+
+( function( $ ) {
+	var body    = $( 'body' ),
+		_window = $( window );
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.scroll-to-top').fadeIn();
+		} else {
+			$('.scroll-to-top').fadeOut();
+		}
+	});
+
+	$('.scroll-to-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
+	// Shrink header on scroll down
+	if($('.site-header').length > 0) {
+		var y = $(window).scrollTop();
+		var padding_element = $('.slider');
+
+		if ( padding_element.length === 0 ) {
+			padding_element = $('#main');
+		}
+
+		if($(window).width() > 979) {
+			masthead_height = $('.site-header').height();
+			masthead_top    = $('.site-header').offset().top+$('.site-header').height()+50; 
+
+			if( y > masthead_top ) { 
+				$('.site-header').addClass('fixed'); 
+				padding_element.css('padding-top', (masthead_height)+'px'); 
+			}
+
+			if( y > 150 ) {
+				$('.site-header').addClass('shrink');
+				$('body').addClass('sticky_header_active');
+			} else {
+				$('.site-header').removeClass('shrink');
+				$('body').removeClass('sticky_header_active');
+			}
+			
+			// Shrink menu on scroll
+			var didScroll = false;
+			$(window).scroll(function() {
+				didScroll = true;
+			});
+
+			setInterval(function() {
+				if ( didScroll ) {
+					didScroll = false;
+					y = $(window).scrollTop();
+
+					if(y > masthead_top){ 
+						$('.site-header').addClass('fixed'); 
+						padding_element.css('padding-top', (masthead_height)+'px'); 
+					}
+					else{ 
+						$('.site-header').removeClass('fixed'); 
+						padding_element.css('padding-top', ''); 
+					}
+					if(y > 500){  
+						$('.site-header').addClass('shrink'); $('body').addClass('sticky_header_active'); 
+					}
+					else{ 
+						$('.site-header').removeClass('shrink'); $('body').removeClass('sticky_header_active'); 
+					}
+				}
+			}, 50);
+		} else {
+			$('.site-header').removeClass('shrink');
+			$('.site-header').removeClass('fixed');
+		}
+
+	} else {
+		$('#page').addClass('static-header'); 
+	}
+
+	// Enable menu toggle for small screens.
+	( function() {
+		var nav = $( '#primary-navigation' ), button, menu;
+		if ( ! nav ) {
+			return;
+		}
+
+		button = nav.find( '.menu-toggle' );
+		if ( ! button ) {
+			return;
+		}
+
+		// Hide button if menu is missing or empty.
+		menu = nav.find( '.nav-menu' );
+		if ( ! menu || ! menu.children().length ) {
+			button.hide();
+			return;
+		}
+
+		$( '.menu-toggle' ).on( 'click.everal', function() {
+			nav.toggleClass( 'toggled-on' );
+		} );
+	} )();
+
+	/*
+	 * Makes "skip to content" link work correctly in IE9 and Chrome for better
+	 * accessibility.
+	 *
+	 * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
+	 */
+	_window.on( 'hashchange.everal', function() {
+		var element = document.getElementById( location.hash.substring( 1 ) );
+
+		if ( element ) {
+			if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+				element.tabIndex = -1;
+			}
+
+			element.focus();
+
+			// Repositions the window on jump-to-anchor to account for header height.
+			window.scrollBy( 0, -80 );
+		}
+	} );
+
+	$( function() {
+		// Search toggle.
+		// $( '.search-toggle' ).on( 'click.everal', function( event ) {
+		// 	var that    = $( this ),
+		// 		wrapper = $( '.search-box-wrapper' );
+
+		// 	that.toggleClass( 'active' );
+		// 	wrapper.toggleClass( 'hide' );
+
+		// 	if ( that.is( '.active' ) || $( '.search-toggle .screen-reader-text' )[0] === event.target ) {
+		// 		wrapper.find( '.search-field' ).focus();
+		// 	}
+		// } );
+
+		/*
+		 * Fixed header for large screen.
+		 * If the header becomes more than 48px tall, unfix the header.
+		 *
+		 * The callback on the scroll event is only added if there is a header
+		 * image and we are not on mobile.
+		 */
+		if ( _window.width() > 781 ) {
+			var mastheadHeight = $( '#masthead' ).height(),
+				toolbarOffset, mastheadOffset;
+
+			if ( mastheadHeight > 48 ) {
+				body.removeClass( 'masthead-fixed' );
+			}
+
+			if ( body.is( '.header-image' ) ) {
+				toolbarOffset  = body.is( '.admin-bar' ) ? $( '#wpadminbar' ).height() : 0;
+				mastheadOffset = $( '#masthead' ).offset().top - toolbarOffset;
+
+				_window.on( 'scroll.everal', function() {
+					if ( ( window.scrollY > mastheadOffset ) && ( mastheadHeight < 49 ) ) {
+						body.addClass( 'masthead-fixed' );
+					} else {
+						body.removeClass( 'masthead-fixed' );
+					}
+				} );
+			}
+		}
+
+		// Focus styles for menus.
+		$( '.primary-navigation, .secondary-navigation' ).find( 'a' ).on( 'focus.everal blur.everal', function() {
+			$( this ).parents().toggleClass( 'focus' );
+		} );
+	} );
+} )( jQuery );
